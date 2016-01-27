381213bbc387ed7ea30a8dd12376536a294745f4
remon <remonasebi@gmail.com>
2016-01-27 01:46:13 +0000

    i thought we could have him marching but it was annoying

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,16 @@ var fs = require('fs');
 
 // returns the path to the word list which is separated by `\n`
 var wordListPath = require('word-list');
-
+//  read in those words
 var wordArray = fs.readFileSync(wordListPath, 'utf8').split('\n');
-// console.log(wordArray)
+// its an empty words array
 var words = [];
+
+// for each word thats in the dictionary (OW memory)
 for (var word in wordArray) {
-  // console.log(wordArray[word].charAt(0))
+  // if it start with d, is HAS to start with d or we don't want it...
   if (wordArray[word].charAt(0) === 'd') {
-    // console.log(wordArray[word])
+    // add it to that empty words array eariler
     words.push(wordArray[word]);
   }
 
@@ -20,6 +22,7 @@ for (var word in wordArray) {
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  // assign those variables badly
   var word1 = words[Math.floor((Math.random() * words.length) + 1)];
   var word2 = words[Math.floor((Math.random() * words.length) + 1)];
   res.render('index', { title: word1 + ' ' + word2});
